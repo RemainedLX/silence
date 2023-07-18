87e7c79b098b838f2ff8a360037fa217e754eb5a
lx <lx_yyds@163.com>
2023-07-18 22:56:24 +0800

    代码生成器（注意依赖问题）

---
+++ service/service-sys/src/main/java/top/remained/silence/ServiceSysApplication.java
@@ -0,0 +1,18 @@
+package top.remained.silence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lx
+ * @CreateTime: 2023-07-18  22:17
+ * @Description: TODO
+ */
+@SpringBootApplication
+public class ServiceSysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSysApplication.class,args);
+    }
+
+}
+
